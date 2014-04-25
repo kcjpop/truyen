f7da72fff0a02485a5b68e5b9da813a9f0c6ab21
kcjpop <kcj.pop.com@gmail.com>
2014-04-25 19:35:28 +0300

    Setup front-end packages.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,13 @@
-$(function() {
+require.config({
+  baseUrl: '/',
+  paths: {
+    'jquery': 'bower_components/jquery/jquery.min',
+    'semantic-ui': 'bower_components/semantic-ui/build/packaged/javascript/semantic.min'
+  }
+});
+
+require(['jquery', 'semantic-ui'], function($) {
+  $(function() {
     $('.popup').popup();
 
     var sidebarChapters = $('.ui.sidebar.chapters')
@@ -11,4 +20,5 @@ $(function() {
     .sidebar('attach events', '.menu.item');
 
     $('.ui.dropdown').dropdown();
+  });
 });
